689ef6df751b07ebdf19aa3e231a3361876c9162
victorboi11 <725304@student.dusd.net>
2023-12-12 11:59:54 -0800

    i did a lot today with my water bottle
    yay

--- P3FlappyBirdEstrada/Assets/Scripts/WaterBottle.cs
+++ P3FlappyBirdEstrada/Assets/Scripts/WaterBottle.cs
@@ -8,10 +8,12 @@ public class WaterBottle : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,11 +25,13 @@ public class WaterBottle : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
             }
         }
     }
     void OnCollisionEnter2D ()
     {
         isDead = true;
+        anim.SetTrigger("Die");
     }
 }
